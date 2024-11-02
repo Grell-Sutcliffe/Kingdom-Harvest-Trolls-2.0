373fa5ea68984c620858b51035b31428845147f0
Jenny Akhtiamova <zhanna.axt@bk.ru>
2024-11-02 03:13:40 +0300

    done!

--- Kingdom-Harvest-Trolls-2.0/Assets/Scripts/Game/ColliderScript.cs
+++ Kingdom-Harvest-Trolls-2.0/Assets/Scripts/Game/ColliderScript.cs
@@ -28,29 +28,7 @@ public class ColliderScript : MonoBehaviour
             {
                 if (collision.gameObject.tag == "Troll")
                 {
-                    Cell cell = fieldScript.cells[index_i, index_j];
-                    Cell new_cell;
-
-                    if (cell.type == "wheat")
-                    {
-                        new_cell = fieldScript.FindCellByType(cell.type, 0, cell.count_of_road, true);
-                    }
-                    else
-                    {
-                        new_cell = fieldScript.FindCellByType(cell.type, cell.level, cell.count_of_road, true);
-                    }
-
-                    new_cell.rotation = cell.rotation;
-                    new_cell = gameController.EditRepairedCell(new_cell);
-
-                    gameController.UpgrateCellInfo(index_i, index_j, new_cell);
-
-                    fieldScript.ChangeCellTag(index_i, index_j, "Untagged");
-
-                    if ((new_cell.type == "castle") && (new_cell.is_destroyed == true) && (new_cell.level > -1))
-                    {
-                        Invoke("CastleSecondChance", 3f);
-                    }
+                    DestroyCell();
                 }
             }
         }
@@ -62,6 +40,33 @@ public class ColliderScript : MonoBehaviour
             }
         }
         gameController.UpdateClaimPanel();
+    }
+
+    private void DestroyCell()
+    {
+        Cell cell = fieldScript.cells[index_i, index_j];
+        Cell new_cell;
+
+        if (cell.type == "wheat")
+        {
+            new_cell = fieldScript.FindCellByType(cell.type, 0, cell.count_of_road, true);
+        }
+        else
+        {
+            new_cell = fieldScript.FindCellByType(cell.type, cell.level, cell.count_of_road, true);
+        }
+
+        new_cell.rotation = cell.rotation;
+        new_cell = gameController.EditRepairedCell(new_cell);
+
+        gameController.UpgrateCellInfo(index_i, index_j, new_cell);
+
+        fieldScript.ChangeCellTag(index_i, index_j, "Untagged");
+
+        if ((new_cell.type == "castle") && (new_cell.is_destroyed == true) && (new_cell.level > -1))
+        {
+            Invoke("CastleSecondChance", 3f);
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -87,6 +92,10 @@ public class ColliderScript : MonoBehaviour
                         gameController.UpgrateCellInfo(index_i, index_j, new_cell);
 
                         fieldScript.ChangeCellTag(index_i, index_j, "Untagged");
+                    }
+                    else if ((cell.is_destroyed == false) && (fieldScript.checks[index_i, index_j].tag == "Knight"))
+                    {
+                        DestroyCell();
                     }
                 }
             }
