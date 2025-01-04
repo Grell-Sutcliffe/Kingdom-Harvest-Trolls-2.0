8e58f07d480d30df98d6efc378e024692caa0d9c
Jenny Akhtiamova <zhanna.axt@bk.ru>
2025-01-04 22:14:01 +0300

    added dropdown

---
+++ Kingdom-Harvest-Trolls-2.0/Assets/Scripts/Difficulty/LanguageChanger.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageChanger : MonoBehaviour
+{
+    public string[] languages;
+    public string current_language;
+}
